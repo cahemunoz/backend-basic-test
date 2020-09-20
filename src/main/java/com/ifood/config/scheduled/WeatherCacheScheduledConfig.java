2b36982b13fa2e47968b710af381578bf938ac81
rafael.fernandes <rafael.fernandes@host.docker.internal>
2020-09-20 02:56:50 -0300

    Ajuste Identação

--- src/main/java/com/ifood/config/scheduled/WeatherCacheScheduledConfig.java
+++ src/main/java/com/ifood/config/scheduled/WeatherCacheScheduledConfig.java
@@ -32,21 +32,20 @@ public class WeatherCacheScheduledConfig {
 		SimpleCacheManager cacheManager = new SimpleCacheManager();
 
 		cacheManager.setCaches(Arrays.asList(
-				new ConcurrentMapCache(Constants.CACHE_WEATHERS,
-										CacheBuilder.newBuilder()
-										.expireAfterWrite(30, TimeUnit.MINUTES)
-										.maximumSize(100)
-										.build()
-										.asMap(), true)));
+				new ConcurrentMapCache(Constants.CACHE_WEATHERS, CacheBuilder.newBuilder()
+																	.expireAfterWrite(30, TimeUnit.MINUTES)
+																	.maximumSize(100)
+																	.build()
+																	.asMap(), true)));
 
 		return cacheManager;
 	}
 
 	
-	  @CacheEvict(allEntries = true, value = {Constants.CACHE_WEATHERS})
-	  @Scheduled(fixedDelay = 10000 * 6, initialDelay = 500) 
-	  public void clearCacheEvict() { 
+	@CacheEvict(allEntries = true, value = {Constants.CACHE_WEATHERS})
+	@Scheduled(fixedDelay = 10000 * 6, initialDelay = 500) 
+	public void clearCacheEvict() { 
 			log.info("Cache limpo em {}!",
 					LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")));
-	  }
+	}
 }
